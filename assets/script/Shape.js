66ee1a1ca83f72b63cdd38bfc5cf18a2e0e3d562
Dale Ruppert <daleruppert@shaw.ca>
2023-11-23 15:26:01 -0600

    pushing to GitHub

---
+++ assets/script/Shape.js
@@ -0,0 +1,20 @@
+'use strict';
+
+export class Shape {
+    constructor(name, color) {
+        this._name = name;
+        this._color = color;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    getInfo() {
+        return `Shape: ${this._name}, Color: ${this._color}`;
+    }
+}
---
+++ assets/script/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import { Shape } from './Shape.js';
+
+const shapeSelect = document.getElementById('shapeSelect');
+const colorSelect = document.getElementById('colorSelect');
+const shapeGrid = document.getElementById('shapeGrid');
+const createButton = document.querySelector('button');
+
+createButton.addEventListener('click', createShape);
+
+function createShape() {
+    const selectedShape = shapeSelect.value;
+    const selectedColor = colorSelect.value;
+
+    const newShape = new Shape(selectedShape, selectedColor);
+
+    const newShapeElement = document.createElement('div');
+    newShapeElement.style.width = '50px';
+    newShapeElement.style.height = '50px';
+
+    if (selectedShape === 'circle') {
+        newShapeElement.classList.add('circle');
+        newShapeElement.style.borderColor = selectedColor;
+    } else if (selectedShape === 'square') {
+        newShapeElement.classList.add('square');
+        newShapeElement.style.backgroundColor = selectedColor;
+    }
+
+    newShapeElement.onclick = function () {
+        alert(newShape.getInfo());
+    };
+
+    shapeGrid.appendChild(newShapeElement);
+}
